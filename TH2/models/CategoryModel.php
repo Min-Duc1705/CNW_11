00de0f4f0f73ce7e627de78c0de55ccd251834fd
hoangdiep-it <hoangdiep2k3@gmail.com>
2024-12-06 01:39:36 +0700

    update categoryModel
    tạo class categoryModel, tạo hàm kết nối SQL + hàm lấy các danh mục

---
+++ TH2/models/CategoryModel.php
@@ -0,0 +1,26 @@
+<?php
+require_once './Config/CategoriesDB.php';
+class CategoryModel {
+    private $conn; // Kết nối cơ sở dữ liệu
+
+    // Hàm khởi tạo để kết nối với cơ sở dữ liệu
+    public function __construct() {
+        $db = new CategoriesDB(); // Tạo đối tượng UserDB
+        $this->conn = $db->getConnection(); // Lấy kết nối PDO
+    }
+
+    // Lấy tất cả các danh mục
+    public function getAllCategories() {
+        $query = "SELECT * FROM categories";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Lấy một danh mục theo ID
+    public function getCategoryById($id) {
+       
+    }
+
+}
+?>
