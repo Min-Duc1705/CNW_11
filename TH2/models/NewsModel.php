a616c1b56d6638b6fb78826411f780c37be5eaed
Min-Duc1705 <146452065+Min-Duc1705@users.noreply.github.com>
2024-12-06 13:41:48 +0700

    commit

--- TH2/models/NewsModel.php
+++ TH2/models/NewsModel.php
@@ -1,25 +1,44 @@
 <?php
-require_once './Config/NewsDB.php';
+require_once '../Config/Database.php';
 class NewsModel {
     private $conn;
 
+
+    // Hoàng Văn Điệp
     // Constructor để kết nối cơ sở dữ liệu
     public function __construct() {
-        $db = new NewsDB(); // Tạo đối tượng UserDB
-        $this->conn = $db->getConnection(); // Lấy kết nối PDO
+        $db = new Database(); // Tạo đối tượng UserDB
+        $this->conn = $db->connect(); // Lấy kết nối PDO
     }
 
     // Lấy danh sách tin tức
-    public function getAllNews() {
+    public function getAllNewsUser() {
         $sql = "SELECT * FROM news ORDER BY created_at DESC";
         $stmt = $this->conn->query($sql);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // Lấy tin tức theo ID
-    public function getNewsById($id) {
+    
+    // Tìm Kiếm
+    public function TimKiem($id) {
       //tim kiem cho nay
     }
+
+
+
+
+    // Lê Minh Đức làm 
+    public function getAllNews() {
+        $stmt = $this->conn->prepare("SELECT news.*, categories.name AS category_name FROM news 
+                                      LEFT JOIN categories ON news.category_id = categories.id");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    
+
+
+
     // Đóng kết nối
     public function closeConnection() {
         $this->conn = null; // Đặt kết nối về null để giải phóng tài nguyên
